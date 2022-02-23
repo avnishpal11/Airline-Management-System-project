321d66088c5a6c7dd3e416cccd4b7b5b22f1d214
avnish <avnishpal8652@gmail.com>
2022-02-23 10:25:25 +0530

    Initial commit

---
+++ src/airline/managment/system/Conn.java
@@ -0,0 +1,17 @@
+package airline.managment.system;
+import java.sql.*;
+public class Conn {
+	Connection c;
+	Statement s;
+	public Conn() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			c= DriverManager.getConnection("jdbc:mysql://localhost:3306/project4","root","root");
+			s= c.createStatement();
+			
+		}catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
